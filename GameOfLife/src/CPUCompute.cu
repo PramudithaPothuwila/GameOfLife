7e0e50fa2a259136726115f0f6da2441fedca3ca
Buddy365 <pramudithapothuwila36@gmail.com>
2022-09-15 00:55:22 +0530

    CPUCompute added getNeighbours function added computeNextGeneration function added
    Signed-off-by: Buddy365 <pramudithapothuwila36@gmail.com>

---
+++ GameOfLife/src/CPUCompute.cu
@@ -0,0 +1,59 @@
+﻿#include "CPUCompute.cuh"
+#include <thread>
+
+namespace GameOfLife
+{
+	void CPUCompute::Compute(int *world)
+	{
+		
+	}
+
+	int CPUCompute::getNeighbours(const std::vector<std::vector<int>> *world, int x, int y)
+	{
+		int neighbours = 0;
+
+		for (int i = -1; i < 2; i++)
+		{
+			for (int j = -1; j < 2; j++)
+			{
+				if (i == 0 && j == 0)
+					continue;
+
+				if (x + i < 0 || x + i >= world->size())
+					continue;
+
+				if (y + j < 0 || y + j >= world->at(0).size())
+					continue;
+
+				if (world->at(x + i).at(y + j) == 1)
+					neighbours++;
+			}
+		}
+		return neighbours;
+	}
+
+	void CPUCompute::computeNextGeneration(std::vector<std::vector<int>> *world)
+	{
+		const auto newWorld = new std::vector<std::vector<int>>(world->size(), std::vector<int>(world->size(), 0));
+		for(int i = 0; i < world->size(); i++)
+		{
+			for(int j = 0; j < world->size(); j++)
+			{
+				const int neighbours = getNeighbours(world, i, j);
+				if(neighbours < 2 || neighbours > 3)
+				{
+					(*newWorld)[i][j] = 0;
+				}
+				else if(neighbours == 3)
+				{
+					(*newWorld)[i][j] = 1;
+				}
+				else
+				{
+					(*newWorld)[i][j] = (*world)[i][j];
+				}
+			}
+		}
+		memcpy(world, newWorld, sizeof *world);//sizeof(std::vector<std::vector<int>>));
+	}
+}
---
+++ GameOfLife/src/CPUCompute.cuh
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include <vector>
+
+
+namespace GameOfLife
+{
+	class CPUCompute
+	{
+	public:
+		static void Compute(int *world);
+	private:
+		static int getNeighbours(const std::vector<std::vector<int>> *world, int x, int y);
+		static void computeNextGeneration(std::vector<std::vector<int>> *world);
+	};
+}
