3191b1e5f2108cbcbb860a3adf14d7a691c12e57
Buddy365 <pramudithapothuwila36@gmail.com>
2022-09-15 09:43:53 +0530

    CPUComputeMultiThread.cu and CPUComputeMultiThread.cuh created.
    Signed-off-by: Buddy365 <pramudithapothuwila36@gmail.com>

---
+++ GameOfLife/src/CPUComputeMultiThread.cuh
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include <vector>
+
+namespace GameOfLife
+{
+	class CpuComputeMultiThread
+	{
+	public:
+		void Compute(std::vector<std::vector<int>> *world);
+	private:
+		void ComputeCell(std::vector<std::vector<int>> *worldSubSector, int x, int y);
+		void ThreadManager(std::vector<std::vector<int>> *world);
+		
+	};
+}
