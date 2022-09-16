4963ea4a43cad8e207822758cbd0e7865ac56ec3
Buddy365 <pramudithapothuwila36@gmail.com>
2022-09-16 11:30:31 +0530

    WorldSlicer functionality added
    Signed-off-by: Buddy365 <pramudithapothuwila36@gmail.com>

--- GameOfLife/src/CPUComputeMultiThread.cu
+++ GameOfLife/src/CPUComputeMultiThread.cu
@@ -1,39 +1,59 @@
 #include "CPUComputeMultiThread.cuh"
-
+#include <thread>
 #include <iostream>
 
 
 namespace GameOfLife
 {
-	void CPUComputeMultiThread::compute(std::vector<std::vector<int>> *world)
+	void CPUComputeMultiThread::compute(grid *world)
 	{
 		
 	}
 
-	void CPUComputeMultiThread::computeCell(std::vector<std::vector<int>> *worldSubSector, int x, int y)
+	void CPUComputeMultiThread::computeCell(grid *worldSubSector, int x, int y)
 	{
 		
 	}
 
-	void CPUComputeMultiThread::worldSlicer(const std::vector<std::vector<int>> *world, int slices)
+	void CPUComputeMultiThread::worldSlicer(const grid *world, const std::vector<grid> *subSectors, int subSectorCount)
 	{
-		auto *worldSlices = new std::vector<std::vector<int>>[slices];
-		int size = world->size();
-
-		std::cout << "Size: " << size << std::endl;
-		
-		// for(int i = 0; i < slices; i++)
-		// {
-		// 	for(int j = -2; j < 2; j++)
-		// 	{
-		// 		worldSlices[i].push_back(world->at(i + j));
-		// 	}
-		// }
-		
+		const int size = world->size();
+		for(int outerX = 2; outerX < size; outerX + 3)
+		{
+			for(int outerY = 2; outerY < size; outerY + 3)
+			{
+				grid subSector;
+				for(int innerX = -2; innerX < 3; innerX++)
+				{
+					std::vector<int> row;
+					for(int innerY = -2; innerY < 3; innerY++)
+					{
+						switch(world->at(outerX + innerX).at(outerY + innerY))
+						{
+							case 0:
+								row.push_back(0);
+								break;
+							case 1:
+								row.push_back(1);
+								break;
+							default:
+								row.push_back(0);
+								break;
+						}
+					}
+					subSector.push_back(row);
+				}
+				//subSectors->push_back(subSector);
+			}
+		}
 	}
 
-	void CPUComputeMultiThread::threadManager(std::vector<std::vector<int>> *world)
+	void CPUComputeMultiThread::threadManager(const grid *world)
 	{
-		
+		const unsigned int processor_count = std::thread::hardware_concurrency();
+		const int subSectorCount = std::ceil(static_cast<double>(world->size() * world->size())/9);
+		std::vector<grid> subSectors;
+		worldSlicer(world, &subSectors, subSectorCount);
+
 	}
 }
