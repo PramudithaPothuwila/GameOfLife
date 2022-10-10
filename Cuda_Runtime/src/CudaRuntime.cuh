b554a262fc61cc691dfdb44c03f4fb976fac18f0
Buddy365 <pramudithapothuwila36@gmail.com>
2022-10-11 00:25:19 +0530

    compute kernel function body added
    ComputeGPU::run added
    ComputeGPU::shutdown added
    Signed-off-by: Buddy365 <pramudithapothuwila36@gmail.com>

--- Cuda_Runtime/src/CudaRuntime.cuh
+++ Cuda_Runtime/src/CudaRuntime.cuh
@@ -1,9 +1,19 @@
 #pragma once
 #include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+enum System_State
+{
+	INIT,
+	RUNNING,
+	SHUTDOWN
+};
+// Global variable
+System_State GPU_SYSTEM_STATE;
 
 namespace Cuda_Runtime
 {
-	__global__ void compute(bool *primary_device_buffer, bool *secondary_device_buffer);
+	__global__ void compute(bool *primary_device_buffer, bool *secondary_device_buffer, int world_width);
 
 	class ComputeGPU
 	{
@@ -11,9 +21,11 @@ namespace Cuda_Runtime
 		bool *primary_device_world_buffer;
 		bool *secondary_device_world_buffer;
 		int world_width;
+		int buffer_size;
 	public:
 		ComputeGPU(int world_width, bool *start_world_frame);
 		void init();
 		void run();
+		void shutdown();
 	};
 }
